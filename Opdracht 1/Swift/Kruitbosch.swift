d89abcdfa2f12144b6c057f4a429c107c5d00a41
thijskruitbosch <113582724+thijskruitbosch@users.noreply.github.com>
2022-09-15 15:08:02 +0200

    Create Kruitbosch.swift

---
+++ Opdracht 1/Swift/Kruitbosch.swift
@@ -0,0 +1,16 @@
+var x:Int = 7
+var c:Int = 1
+if x >= 0 {
+    while (c <= x){
+    if c % 2 == 0{
+        print("Hallo Kruitbosch");
+    }
+    else {
+        print("Doei Kruitbsoch");
+    }
+    c+=1
+    }
+    
+} else {
+    print("Use a value greater than 0")
+}
